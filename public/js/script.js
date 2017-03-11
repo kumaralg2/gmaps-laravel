3a556fa4ed272b5a5d297acd909e8c1e3209a5b3
Kiran <kumar.alg2@gmail.com>
2017-03-12 01:51:35 +0530

    - creating functions - making the code reusable - checking the browser to enable geolocation with success and fail functions

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,67 @@
 
+var map;
+var myLatLng;
 $(document).ready(function () {
-    var pyrmont = new google.maps.LatLng(-33.8665433, 151.1956316);
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: pyrmont,
-        zoom: 16
-    });
+    geoLocationInit();
+    function geoLocationInit() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success, fail);
+        } else {
+            alert("Browser not supported");
+        }
+    }
+
+    function success(position) {
+        var latval = position.coords.latitude;
+        var lngval = position.coords.longitude;
+        myLatLng = new google.maps.LatLng(latval, lngval);
+        console.log(latval);
+        console.log(lngval);
+        console.log(myLatLng);
+        createMap(myLatLng);
+        nearbySearch(myLatLng, "school")
+    }
+
+    function fail() {
+        alert("it fails");
+    }
+
+    function createMap(myLatLng) {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: myLatLng,
+            zoom: 16
+        });
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map
+        });
+    };
+    function nearbySearch(myLatLng, type) {
+        var request = {
+            location: myLatLng,
+            radius: '500',
+            types: [type]
+        };
+
+        service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, callback);
+
+        function callback(results, status) {
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                for (var i = 0; i < results.length; i++) {
+                    var place = results[i];
+                    latlng = place.geometry.location;
+                    icn = place.icon;
+                    name = place.name;
+                    createMarker(latlng, icn, name);
+                }
+                console.log(results);
+            }
+        }
+    }
+
     //Create a Marker
-    function createMarker(latLng, icon) {
+    function createMarker(latLng, icon, name) {
         var marker = new google.maps.Marker({
             position: latLng,
             map: map,
@@ -15,30 +70,11 @@ $(document).ready(function () {
                 anchor: new google.maps.Point(10, 10),
                 scaledSize: new google.maps.Size(10, 17)
             },
-            title: 'location'
+            title: name
         });
 
     }
-    var request = {
-        location: pyrmont,
-        radius: '500',
-        types: ['store']
-    };
 
-    service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, callback);
-
-    function callback(results, status) {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                var place = results[i];
-                latlng = place.geometry.location;
-                icn = place.icon;
-                createMarker(latlng, icn);
-            }
-            console.log(results);
-        }
-    }
 
 
 });
